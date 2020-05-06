0b61bd8cf3d0df534765529b6baf28f53861b487
amgillenwater <amgillenwater@gmail.com>
2020-05-06 12:54:19 -0400

    got image to show up, parent element error fixed

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 function renderPageContent(){
     let customer = customers[0]
-    let targetElement = document.querySelector(".card")
+    let targetElement = document.querySelector(".container")
     // console.log(targetElement)
     // console.log(customer.cell)
 
     let cardElement = document.createElement("div");
     cardElement.className = "card";
+    targetElement.appendChild(cardElement);
     // console.log(cardElement)
-    let photoElement = document.createElement("img");
+    let photoElement = document.createElement("div");
+    photoElement.className = "photo"
     photoElement.innerHTML = `<img src="${customer.picture.thumbnail}">`;
-    // console.log (photoElement)
+    targetElement.appendChild(photoElement);
+    console.log (photoElement)
+    let infoElement = document.createElement("div")
+    infoElement.className = "info";
+
 
 
 }
