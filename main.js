43ef833dbba993ae5c72be7eb6addd61f16d8d82
amgillenwater <amgillenwater@gmail.com>
2020-05-06 13:01:13 -0400

    added some info, just need to repeat for each piece of info needed

--- main.js
+++ main.js
@@ -5,16 +5,24 @@ function renderPageContent(){
     // console.log(customer.cell)
 
     let cardElement = document.createElement("div");
-    cardElement.className = "card";
-    targetElement.appendChild(cardElement);
+        cardElement.className = "card";
+        targetElement.appendChild(cardElement);
     // console.log(cardElement)
     let photoElement = document.createElement("div");
-    photoElement.className = "photo"
-    photoElement.innerHTML = `<img src="${customer.picture.thumbnail}">`;
-    targetElement.appendChild(photoElement);
-    console.log (photoElement)
+        photoElement.className = "photo";
+        photoElement.innerHTML = `<img src="${customer.picture.thumbnail}">`;
+        cardElement.appendChild(photoElement);
+    // console.log (photoElement)
     let infoElement = document.createElement("div")
-    infoElement.className = "info";
+        infoElement.className = "info";
+        cardElement.appendChild(infoElement);
+    let nameElement = document.createElement("div")
+        nameElement.className = "name";
+        nameElement.innerHTML = `${customer.name.first} ${customer.name.last}`
+    // console.log(nameElement)
+        cardElement.appendChild(nameElement);
+    
+
 
 
 
