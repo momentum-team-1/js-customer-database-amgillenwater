676388c00fde30c59b6fc6b99c476f7e5f160eac
amgillenwater <amgillenwater@gmail.com>
2020-05-06 12:39:28 -0400

    created a div with a class

---
+++ main.js
@@ -0,0 +1,13 @@
+function renderPageContent(){
+    let customer = customers[0]
+    let targetElement = document.querySelector(".card")
+    // console.log(targetElement)
+    // console.log(customer.cell)
+
+    let cardElement = document.createElement("div");
+    cardElement.className = "card";
+    console.log(cardElement)
+
+}
+
+renderPageContent()
