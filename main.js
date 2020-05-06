299aa72ad603bacb04ddd3ee568e174abc700df3
amgillenwater <amgillenwater@gmail.com>
2020-05-06 14:26:16 -0400

    wow actually made a thing work

--- main.js
+++ main.js
@@ -39,4 +39,51 @@ function renderPageContent(){
         cardElement.appendChild(regDate);
 }
 
-renderPageContent()
+
+function doingIt() {
+    let targetElement = document.querySelector(".container")
+
+    for (let individual of customers){
+        // console.log(individual);
+        let cardElement = document.createElement("div");
+        cardElement.className = "card";
+        targetElement.appendChild(cardElement);
+        // photo:
+        let photoElement = document.createElement("div");
+        photoElement.className = "photo";
+        photoElement.innerHTML = `<img src="${individual.picture.thumbnail}">`;
+        cardElement.appendChild(photoElement);
+        //info div:
+        let infoElement = document.createElement("div")
+        infoElement.className = "info";
+        cardElement.appendChild(infoElement);
+        //name:
+        let nameElement = document.createElement("div")
+        nameElement.className = "name";
+        nameElement.innerHTML = `<h2>${individual.name.first} ${individual.name.last}</h2>`
+        cardElement.appendChild(nameElement);
+        // address line 1
+        let addressElement = document.createElement("div");
+        addressElement.className = "address-street";
+        addressElement.innerHTML = `${individual.location.street}`;
+        cardElement.appendChild(addressElement);
+        // address line 2:
+        let addressTwoElement = document.createElement("div");
+        addressTwoElement.className = "city-state-zip";
+        addressTwoElement.innerHTML = `${individual.location.city} ${individual.location.state} ${individual.location.postcode}`;
+        cardElement.appendChild(addressTwoElement);
+        //phone number:
+         let phoneElement = document.createElement("div");
+        phoneElement.className = "phone";
+        phoneElement.innerHTML = `${individual.phone}`;
+        cardElement.appendChild(phoneElement);
+        //registration date:
+        let regDate = document.createElement("div");
+        regDate.className = "reg-date";
+        regDate.innerHTML=`${individual.registered}`;
+        cardElement.appendChild(regDate);
+
+    }
+}
+
+doingIt()
